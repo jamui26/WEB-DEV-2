d5149be82b7b7848b91c9927ed701f4cc393f3e8
jamui26 <komaljamui26@gmail.com>
2026-01-16 12:47:22 +0530

    JS-Day-1,2

---
+++ JS-Day-1/script.js
@@ -0,0 +1,48 @@
+//Type of function
+
+//1.regular function
+// function sample(){
+//     console.log(a, "sample");
+// }
+// sample(5)
+
+//2.arrow function
+//It doesnot need return statement and if follows asynchronous operation
+
+
+// const sample=()=>"sample"
+// console.log(sample(5,10))
+
+// const sample=(a,b)=>a+b;
+// console.log(sample(2,3));
+
+// const sample=(a,b)=>{
+//     return a+b;
+// }
+// console.log(sample(5,10))
+
+
+
+// Asynchronous function : we can  
+
+
+
+//3.callback function
+//In callback function if we pass acallback function as an arugument inside another function.
+
+
+// callbackd=demo()
+// function sample(callback){
+//     console.log("sample");
+//     callback();
+// }
+
+// function sample(){
+//     console.log("sample");
+// }
+// sample()
+// function demo(){
+//     console.log("demo");
+// } 
+// sample(demo)   
+
